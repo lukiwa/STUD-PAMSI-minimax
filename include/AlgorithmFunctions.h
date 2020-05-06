eb1c44a537e71aad0905d13b0274aef09d14ec69
liwan <lukasz.iwan.99@gmail.com>
2020-05-06 18:23:12 +0200

    Beggining working on minimax

---
+++ include/AlgorithmFunctions.h
@@ -0,0 +1,8 @@
+//
+// Created by Lukasz-Lap on 06.05.2020.
+//
+
+#pragma once
+
+
+int MiniMax(int a);
---
+++ src/AlgorithmFunctions.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Lukasz-Lap on 06.05.2020.
+//
+#include "AlgorithmFunctions.h"
+
+
+int MiniMax(int a) {
+    return 15;
+}
